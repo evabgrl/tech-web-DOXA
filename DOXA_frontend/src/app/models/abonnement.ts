f629ab3438e801eb992eae9a5157c6b142e371db
evaaa <117753781+evabgrl@users.noreply.github.com>
2024-05-12 18:55:25 +0200

    Changement de skeleton - WIP

--- DOXA_frontend/src/app/models/abonnement.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user';
-import { Notification } from './notification';
-
-export interface Abonnement {
-  idFollowership: number; // Identifiant du suivi
-  userChecked: User; // Utilisateur suivi
-  userFollower: User; // Utilisateur suiveur
-  notifications: Notification[]; // Liste des notifications liées à cet abonnement
-}
--- DOXA_frontend/src/app/models/message.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user';
-
-export interface Message {
-  idMessage: number; // Identifiant du message
-  text: string; // Contenu du message
-  userTransmitter: User; // Utilisateur ayant envoyé le message
-  userReceiver: User; // Utilisateur ayant reçu le message
-  date: Date; // Date d'envoi du message
-}
